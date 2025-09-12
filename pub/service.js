1075dad4b66071df1c78e0d353ec52f063d4af55
tsvens <tomas.svensson@ibm.com>
2025-09-12 10:48:12 +0200

    Initial Commit

---
+++ pub/service.js
@@ -0,0 +1,17 @@
+app.service('myserv', function() {
+          this.getServiceList = function () {
+    return [];
+}
+this.getISEndpoint = function() { 
+ return 'http://IBM-PF39GCDY:35555/';
+}
+this.getAPIList = function() { 
+ return [];
+}
+this.getCreatedTime = function() { 
+ return "01-09-2025 15:52:45 CEST";
+}
+this.getPackageInfo = function(){
+ return{"packageName":"Tomas","createdDate":"01-09-2025 14:16:19 CEST","version":"1.0"};
+}
+});
